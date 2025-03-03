5901ac5728961d84b877912936a6010db3316d4a
Administrator <Administrator@D1-MERKUR-TR>
2025-03-03 13:45:24 +0100

    Projektdateien hinzufügen.

---
+++ GeneralSnippets/RValueLValue/RValueLValue.cpp
@@ -0,0 +1,86 @@
+// =====================================================================================
+// LValue / RValue
+// =====================================================================================
+
+module modern_cpp:rvalue_lvalue;
+
+namespace LValueRValue {
+
+    // lvalue reference
+    static void sayHello(std::string& message) {
+        std::println("sayHello [std::string&]:  {}", message);
+    }
+
+    // rvalue reference
+    static void sayHello(std::string&& message) {
+        std::println("sayHello [std::string&&]: {}", message);
+    }
+
+    static void test01() {
+
+        std::string a = "Hello";     // Stack
+        std::string b = " World";
+
+        std::string tmp = a + b;
+
+        sayHello(a);
+        sayHello(a + b);  // Konkatenation: "Hello World"
+    }
+
+    // -------------------------------------------------------------------
+
+    static void helper(std::string&& message) {  // RValue
+
+        sayHello( std::move(message));    // A) sayHello (&&):  message ist vom Typ std::string&&
+                              // B) sayHello (&): Das an. Objekt hat einen Namen 'message' bekommen !!!
+
+    }
+
+    static void test02() {
+
+        helper(std::string("Where are we going ..."));
+    }
+
+    // -------------------------------------------------------------------
+
+    static void test03() {
+
+        std::string s = "Hello";
+
+        sayHello(s);
+        // versus
+        sayHello(std::move(s));  // casts an lvalue to an rvalue
+    }
+
+    // -------------------------------------------------------------------
+
+    static void test04() {
+
+        // int& i = 2;          // invalid: reference to a constant
+
+        // int a = 2;
+        // int b = 3;
+
+        // int& k = a + b;     // invalid: reference to a temporary
+
+        int&& i = 2;           // works: reference to a constant
+
+        int a = 2;
+        int b = 3;
+
+        int&& k = a + b;       // works: reference to a temporary
+    }
+}
+
+void main_rvalue_lvalue() 
+{
+    using namespace LValueRValue;
+    test01();
+    test02();
+    test03();
+    test04();
+}
+
+// =====================================================================================
+// End-of-File
+// =====================================================================================
---
+++ Readme.txt
@@ -0,0 +1,112 @@
+Modern C++
+
+Peter Loos
+
+Dev. // Schulung
+
+C/C++ Java  C#  JavaScript Android (Java) ( Python )
+
+Back-to-the-Roots: C/C++
+
+Guten Morgen
+
+== Visual Studio 2022
+
+== Github
+
+Vorstellungsrunde:
+
+= Was sind meine Erwartungen
+
+= Was muss DRIN sein
+
+= Benötige einen Überblick über alles "Moderne"
+
+= Mein Background: Welche Sprachen
+
+=============================================
+
+9  => 17 Uhr
+
+Kantine:  12.30
+
+caaaaaa. 10.30 // 15.00
+
+=============================================
+
+Lambdas , Templates , MT, alles Neue
+
+gut strukturierter Kurs  
+
+C => C++:   OO  Klasse ....
+
+DRY  Don't repeat yourself 
+
+emplace
+
+=============================================
+
+C# versus C++:
+
+A) C#:   Referenz-Semantik
+
+B) C++:  Value-Semantik
+         Referenz-Semantik
+
+class Time{}
+
+void tueWas ()
+{
+    Time now;                // C++ // Stack
+
+    Time now =  new Time();  // C# // now: Referenz
+
+    Time* now = new Time();  // C++ // Heap
+    delete now;
+}
+
+===========================
+
+Begriff der Referenz:
+
+Oberflächlich:  Adresse
+
+C#:   ==> new // Halde // Exakt: Doppel-Zeiger: Stub => Heap
+
+C++:  ==> Ist ein Aliasname eines vorhandenen Objekts (Variable)
+
+          Intern: Durch die Adresse des Originalobjekts //
+                                    des referenzierten Objekts
+
+C-Level:
+
+a = 123;
+a = b;
+
+lvalue:  left value   // Adresse
+rvalue:  right value  // Wert
+
+
+=============================================================
+
+:--: ... BE0   // Original  // anonym. Objekt am Stack // tmp. Objekt  // wird freigegeben
+:--: ... 120   // Kopie im Vektor
+
+:--: ... A50
+
+=============================================================
+
+Why auto vor Funktionen ???
+
+Welches Paradigma ist in C++ gegeben ???
+
+OO: Objektorientierung
+GP: Great Performanz
+
+GP: Generische Programmierung (Templates)
+
+    Alexander Alexandrowitsch Stepanow // Stroustrup ==> Bibliothek: STL
+
+"Mit Typen rechnen"  ==> Metaprogramming
+
+=============================================================
